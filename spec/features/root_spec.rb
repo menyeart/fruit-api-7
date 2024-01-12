1abbd8de47919708192d19f3c647dcda94d0f5dd
Matt Enyeart <menyeart1@gmail.com>
2024-01-12 11:54:06 -0700

    Create root feature spec

---
+++ spec/features/root_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "Root Page" do
+  describe "As a user" do
+    it "When I visit the root path ("/"),I see a form with a search box for fruit and a button. When I enter the name of a fruit and click 'Search' I am taken to the '/foods' page and see all of its atrributes."
+
+
+    # - name
+    # - family
+    # - order
+    # - genus
+    # - nutrition value, organized as: 
+    #   "Calories: x
+    #   Fat: x
+    #   Sugar: x
+    #   Carbohydrates: x
+    #   Protein: x
+    #   "
+  end
+
+
+end
