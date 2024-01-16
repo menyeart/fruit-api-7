9f5817b3b43270fb9fc75f61cad83575a0b9b606
Matt Enyeart <menyeart1@gmail.com>
2024-01-16 08:51:08 -0700

    Implement test for user story 1

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -1,8 +1,24 @@
 require "rails_helper"
 
-RSpec.describe "Root Page" do
+RSpec.describe "Root Page", type: :feature do
   describe "As a user" do
-    it "When I visit the root path ("/"),I see a form with a search box for fruit and a button. When I enter the name of a fruit and click 'Search' I am taken to the '/foods' page and see all of its atrributes."
+    it "When I visit the root path ('/'),I see a form with a search box for fruit and a button. When I enter the name of a fruit and click 'Search' I am taken to the '/foods' page and see all of its atrributes." do
+
+      visit "/"
+
+      expect(page).to have_content("Search for Fruit")
+      expect(page).to have_button("Search")
+
+      fill_in "search", with:"baNana"
+      click_button("Search")
+  
+      expect(current_path).to eq("/fruits")
+      expect(page).to have_content("Banana")
+      expect(page).to have_content("Family: Musaceae")
+      expect(page).to have_content("Genus: Musa")
+      expect(page).to have_content("Name: Banana")
+      expect(page).to have_content("Order: Zingiberales")
+
 
 
     # - name
@@ -16,7 +32,6 @@ RSpec.describe "Root Page" do
     #   Carbohydrates: x
     #   Protein: x
     #   "
+    end
   end
-
-
 end
