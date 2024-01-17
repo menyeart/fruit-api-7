de0047565e6feceada7f7b86e2b65ae5dc4cab28
Matt Enyeart <menyeart1@gmail.com>
2024-01-17 09:31:33 -0700

    Add logic to fruits facade for getting list of similar fruits

--- app/facades/fruits_facade.rb
+++ app/facades/fruits_facade.rb
@@ -1,15 +1,26 @@
 class FruitsFacade
   attr_reader :fruit_name
-  def initialize(fruit_name)
-    @fruits = FruitService.new.all_fruits
+  def initialize(fruit_name = "banana")
     @fruit_name = fruit_name.downcase.capitalize
   end
 
+  def all_fruits
+    FruitService.new.all_fruits
+  end
+
   def get_fruit
-    @fruits.find { |fruit| fruit[:name] == "Banana" }
+    all_fruits.find { |fruit| fruit[:name] == @fruit_name }
   end
 
   def create_fruit_obj
     Fruit.new(get_fruit)
+  end
+
+  def get_similar_fruits(calories)
+    sim_fruits = FruitService.new.similar_fruits(calories)
+  end
+
+  def create_similar_fruits(calories)
+    get_similar_fruits(calories).map { |fruit| Fruit.new(fruit)}
   end
 end
