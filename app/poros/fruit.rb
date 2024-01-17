9a35b3f29f5149d0141601fa3adac6649ae23d13
Matt Enyeart <menyeart1@gmail.com>
2024-01-17 09:32:04 -0700

    Add nutrition info attribute and attr reader

--- app/poros/fruit.rb
+++ app/poros/fruit.rb
@@ -1,11 +1,11 @@
 class Fruit
-  attr_reader :name, :family, :order, :genus
+  attr_reader :name, :family, :order, :genus, :nutrition_info
   def initialize(fruit_info)
     @name = fruit_info[:name]
     @family = fruit_info[:family]
     @order = fruit_info[:order]
     @genus = fruit_info[:genus]
-    @nutirtion_info = fruit_info[:nutritions]
+    @nutrition_info = fruit_info[:nutritions]
   end
 
 end
