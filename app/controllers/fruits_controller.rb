7d31bb8f6fabf3c60b51a205f66a3c18f2417a82
Matt Enyeart <menyeart1@gmail.com>
2024-02-08 09:12:05 -0700

    Refactor index method to call fruits facade with no arg and creat fruit obj with arg

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -1,11 +1,11 @@
 class FruitsController < ApplicationController
 
   def index
-    @fruit = FruitsFacade.new.create_fruit_obj
+    @fruit = FruitsFacade.new.create_fruit_obj(params["search"])
   end
 
   def similar_fruits
-    @similar_fruits = FruitsFacade.new().create_similar_fruits(params["nutrition"]["calories"])
+    @similar_fruits = FruitsFacade.new.create_similar_fruits(params["nutrition"]["calories"])
   end
 
 end
